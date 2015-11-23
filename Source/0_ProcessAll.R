6906e65f5a9f5074c83e0524713b79bd84ae2a1f
Erik Jan de Vries <erikjandevries@hotmail.com>
2015-11-23 16:19:39 +0100

    Updated 0_ProcessAll.R

--- Source/0_ProcessAll.R
+++ Source/0_ProcessAll.R
@@ -7,11 +7,16 @@ source("Source/2_SetParameters.R");
 # Read the data source files
 source("Source/3_ReadData.R");
 
-# Clean the total data set
+# Clean the data
 source("Source/4_CleanData.R");
+# source("Source/4.1_CheckSums.R");
+source("Source/4.2_CorrectErrors.R");
 
 # Analyse
 source("Source/5_Analyse.R");
 
 # Visualise
 source("Source/6_Visualise.R");
+
+# Finalise
+source("Source/7_Finalise.R");
