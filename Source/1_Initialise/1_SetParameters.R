47451edba9383a2eb996f418d9d5becb6cb37eec
Erik Jan de Vries <erikjandevries@hotmail.com>
2016-03-04 15:01:44 +0100

    Code cleanup

--- Source/1_Initialise/1_SetParameters.R
+++ Source/1_Initialise/1_SetParameters.R
@@ -10,17 +10,23 @@ set.seed(project_parameters$seed);
 # Date time stamp
 project_parameters$DTS  <- as.POSIXlt(Sys.time(), "UTC");
 project_parameters$DTSName = format(project_parameters$DTS, "%Y-%m-%dT%H-%M-%SZ")
-# print(paste("Current UTC time is:", project_parameters$DTSName));
 
 # Folders
 project_parameters$outputBaseFolder  <- "Output";
 project_parameters$folders <- list(
-    data   = "data"
+    data   = "Data"
   , output = paste(project_parameters$outputBaseFolder, project_parameters$DTSName, sep="/")
+  , rds    = paste(project_parameters$outputBaseFolder, "RDS", sep="/")
   );
-ifelse(!dir.exists(file.path(getwd(), project_parameters$outputBaseFolder)), dir.create(file.path(getwd(), project_parameters$outputBaseFolder)), FALSE);
-ifelse(!dir.exists(file.path(getwd(), project_parameters$folders$data)),     dir.create(file.path(getwd(), project_parameters$folders$data)), FALSE);
-ifelse(!dir.exists(file.path(getwd(), project_parameters$folders$output)),   dir.create(file.path(getwd(), project_parameters$folders$output)), FALSE);
+
+project_ensure_folder <- function(folderName) {
+  ifelse(  !dir.exists(file.path(getwd(), folderName))
+          , dir.create(file.path(getwd(), folderName), recursive = TRUE)
+          , FALSE);
+}
+project_ensure_folder(project_parameters$folders$data);
+project_ensure_folder(project_parameters$folders$output);
+project_ensure_folder(project_parameters$folders$rds);
 
 # Data files
 project_parameters$files <- list(
