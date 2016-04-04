977dfbdd925c562e388d975136402c08e891ee57
Erik Jan de Vries <erikjandevries@hotmail.com>
2016-04-04 15:15:40 +0200

    Update: Automatically install packages if they are not yet installed.

--- Source/2_LoadLibraries/_LoadLibraries.R
+++ Source/2_LoadLibraries/_LoadLibraries.R
@@ -1,44 +1,35 @@
-library(shiny);             # R web app
-library(mice);              # impute
-library(caTools);           # sample.split
-library(scales);            # percent formatting
+packages <- rownames(installed.packages());
 
-# print("Installing packages...")
-# install.packages("caTools");
-# install.packages("mice");
-# install.packages("ROCR");
-# install.packages("rpart");
-# install.packages("rpart.plot");
-# install.packages("randomForest");
-# install.packages("caret");
-# install.packages("e1071");
-# install.packages("tm");
-# install.packages("SnowballC");
-# install.packages("RTextTools");
-# install.packages("flexclust");
-# install.packages("ggplot2")
-# install.packages("dplyr");
-# install.packages("reshape2");
-# install.packages("scales");
-# install.packages("lubridate");
-# install.packages("shiny");
+required.packages <- c(
+    "shiny"                 # R web app
+  , "mice"                  # impute
+  , "caTools"               # sample.split
+  , "scales"                # percent formatting
 
-# print("Loading libraries...")
-# library(ROCR);              # auc
-# library(rpart);             # CART
-# library(rpart.plot);        # plot CART
-# library(randomForest);      # random forest
-# library(nnet);              # neural network
-# library(caret);             # cross-validation for CART, preProcess
-# library(e1071);             # cross-validation for CART
-# library(tm);                # text mining
-# library(SnowballC);         #
-# library(RTextTools);        # poly-grams
-# library(flexclust);         # clustering
-# library(ggplot2);
-# library(dplyr);
-# library(reshape2);          # melt
-# library(lubridate);
+  # , "ROCR"                # auc
+  # , "rpart"               # CART
+  # , "rpart.plot"          # plot CART
+  # , "randomForest"        # random forest
+  # , "nnet"                # neural network
+  # , "caret"               # cross-validation for CART, preProcess
+  # , "e1071"               # cross-validation for CART
+  # , "tm"                  # text mining
+  # , "SnowballC"           #
+  # , "RTextTools"          # poly-grams
+  # , "flexclust"           # clustering
+  # , "ggplot2"
+  # , "dplyr"
+  # , "reshape2"            # melt
+  # , "lubridate"
+);
+
+for (p in required.packages) {
+  if (!(p %in% packages)) {
+    install.packages(p);
+  }
+  library(p, character.only = TRUE);
+}; rm(p);
+rm(packages, required.packages);
 
 
 # print("Loading custom functions...");
